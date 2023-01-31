dafe39877de37d1a8138db92e678bea2ab357ce7
Denis Fedorov <ssboshen@yandex.ru>
2023-02-01 00:15:02 +0300

    Fix bug with /filter & /main pages trouble

--- src/main/java/medved/java/sweater/controller/MainController.java
+++ src/main/java/medved/java/sweater/controller/MainController.java
@@ -6,6 +6,7 @@ import medved.java.sweater.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,8 +27,14 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
-        model.put("messages", messageRepository.findAll());
+    public String main(@RequestParam(required = false, defaultValue = "") String filter,
+            Model model) {
+        if (filter != null && !filter.isEmpty()) {
+            model.addAttribute("messages", messageRepository.findByTag(filter));
+        } else {
+            model.addAttribute("messages", messageRepository.findAll());
+        }
+        model.addAttribute("filter",filter);
         return "main";
     }
 
@@ -43,16 +50,16 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("/filter")
-    public String filterByTag(
-            @RequestParam String filter,
-            Map<String, Object> model
-    ) {
-        if (filter != null && !filter.isEmpty()) {
-            model.put("messages", messageRepository.findByTag(filter));
-        } else {
-            model.put("messages", messageRepository.findAll());
-        }
-        return "main";
-    }
+//    @PostMapping("/filter")
+//    public String filterByTag(
+//            @RequestParam String filter,
+//            Map<String, Object> model
+//    ) {
+//        if (filter != null && !filter.isEmpty()) {
+//            model.put("messages", messageRepository.findByTag(filter));
+//        } else {
+//            model.put("messages", messageRepository.findAll());
+//        }
+//        return "main";
+//    }
 }
